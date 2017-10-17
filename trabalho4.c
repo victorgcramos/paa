ff1b33b6aaaa8873b8f8afc14148e40570442eef
Victor Guedes <victorgcramos@gmail.com>
2017-10-17 20:05:28 -0200

    shit

--- trabalho4.c
+++ trabalho4.c
@@ -5,6 +5,7 @@
 
 
 int a,b,c,d,e,ff,g,*tam, cont_tam=1, tam_esq =0, tam_dir=0;
+int *trabalhos_niveis;
 FILE *fp;
 
 int f (int n, int g) // conta numero iterações de f(n).
@@ -28,8 +29,18 @@ void registra_trabalho_nivel_atual_arvore(int nivel, time_t tempo){
   printf("Trabalho por nivel = %d: %d\n", nivel, tempo);
 }
 
-int Calcula_nivel_atual_arvore (int esq, int dir){
-
+int Calcula_nivel_atual_arvore () {
+  if (tam_dir>tam_esq) {
+    tam_dir--;
+    return tam_dir;
+  } else if (tam_esq>tam_dir) {
+    tam_esq--;
+    return tam_esq;
+  } else {
+    tam_esq--;
+    tam_dir--;
+    return tam_esq;
+  }
 }
 
 void func_t (int n) // simula a recorrencia
@@ -38,18 +49,18 @@ void func_t (int n) // simula a recorrencia
   if (n<=1) return;
 
   for (int cont=1; cont<=a; cont++) {// faz a chamadas de tamanho (n/b) - c
-    printf("Esquerda: %d\n",  n/b - c);
+    // printf("Esquerda: %d\n",  n/b - c);
     tam_esq ++;
     func_t( (n/b) - c );
   }
   for (int cont=1; cont<=d; cont++){ // faz d chamadas de tamanho (n/e) - f
-    printf("direita: %d\n",  n/e - ff);
+    // printf("direita: %d\n",  n/e - ff);
     tam_dir++;
     func_t( (n/e) - ff );
   }
 
-  cont_tam = tam_dir > tam_esq ? tam_dir : tam_esq;
-
+  int nivel = Calcula_nivel_atual_arvore();
+  printf("direita: %d, esquerda: %d,Nível: %d\n",tam_dir,tam_esq, nivel);
   int fn = f(n,g);
   time(&tempo_inicial);
   for (int cont=1; cont<= f(n,g); ){ // simula o tempo de f(n) no nível atual
@@ -59,7 +70,7 @@ void func_t (int n) // simula a recorrencia
 
   tempo = difftime(tempo_final, tempo_inicial);
   // int nivel = Calcula_nivel_atual_arvore(tam_esq, tam_dir)
-  // registra_trabalho_nivel_atual_arvore(nivel,tempo);
+  registra_trabalho_nivel_atual_arvore(nivel,tempo);
   registra_trabalho_por_n(n,tempo);
   registra_trabalho_por_f_n(fn,tempo);
 }
@@ -74,7 +85,18 @@ int main(int argc, char const *argv[]) {
   ff = strtol(argv[6],&ff,10);
   g = strtol(argv[7],&g,10);
   n = strtol(argv[8],&n,10);
-  // cont_tam = a
+
+  if (b==1) {
+    cont_tam = n/c;
+  } else if (e==1) {
+    cont_tam = n/e;
+  } else if (b>e) {
+    cont_tam = log(n)/log(e);
+  } else {
+    cont_tam = log(n)/log(b);
+  }
+  printf("Tamanho da arvore: %d\n", cont_tam);
+  trabalhos_niveis = malloc(sizeof(time_t)*cont_tam);
 
   printf("a=%d,b=%d,c=%d,d=%d,e=%d,f=%d,g=%d,n=%d,\n",a,b,c,d,e,ff,g,n);
   // printf("%d\n", a);
